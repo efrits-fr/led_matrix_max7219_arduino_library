619ec07d317d61b0d6b485e7b4325cb3297ba433
Brillante Jason <damdoshi@hotmail.com>
2022-01-01 21:50:04 +0100

    Change demo for a single ball. Add commentaries and generate only HTML for doxygen.
    * Generate documentation into the docs/ directory, exclusively using HTML.
    
    * Set a single ball for the demo animation. Add commentaries.
    
    * Fix typos in comments

--- sources/examples/8x32_led_matrix_max7219/8x32_led_matrix_max7219.ino
+++ sources/examples/8x32_led_matrix_max7219/8x32_led_matrix_max7219.ino
@@ -8,42 +8,40 @@
 
 #include <ledMatrixMax7219Control.h>
 
-enum { NbBalls = 3 };
-enum { CountMax = 500 };
-
+// The representation of the screen in memory
 ledMatrixMax7219Control matrix;
 
+// Variables used for the animation
 uint8_t countLoop = 0;
+float ballX;
+float ballY;
+float ballXSpeed;
+float ballYSpeed;
+// How many loops are required before doing a screen clear again
+enum { CountMax = 500 };
 
-typedef struct s_ball
-{
-  float x;
-  float y;
-  float xSpeed;
-  float ySpeed;
-} t_ball;
-
-t_ball balls[NbBalls];
-
-void setupBall(t_ball * ball)
-{
-  ball->x = random(matrix.getScreenWidth());
-  ball->y = random(matrix.getScreenHeight());
-  ball->xSpeed = random(-10, 10) / 10.0;
-  ball->ySpeed = random(-10, 10) / 10.0;
-}
-
+This function is only called once at startup to set up the demo
 void setup()
 {
+  // Initialization of the matrix, allow it to work correctly
   matrix.init();
+  // Fill the screen with activated pixels
   matrix.clearScreen(1);
-  
-  randomSeed(analogRead(0));
-  for (uint8_t i = 0; i < NbBalls; ++i)
-  {
-    setupBall(&balls[i]);
-  }
 
+  // Set a seed from an analog entry so the random generator generates differents values at each startup
+  randomSeed(analogRead(0));
+
+  // Set the horizontal ball position somewhere between 0 and the width of the screen randomly
+  ballX = random(matrix.getScreenWidth());
+  // Set the vertical ball position somewhere between 0 and the height of the screen randomly
+  ballY = random(matrix.getScreenHeight());
+  // Set an horizontal and vertical speed between -1 and 1, but not inside -0.2 0.2 so the
+  // ball does not follow perfectly horizontal or vertical lines
+  while (fabs(ballXSpeed = random(-10, 10) / 10.0) < 0.2);
+  while (fabs(ballYSpeed = random(-10, 10) / 10.0) < 0.2);
+
+  // Set different brightness levels of matrix, so a little variety appears when balls move from
+  // a matrix to another one.
   matrix.setBrightnessOfLEDMatrix(ledMatrixMax7219Control::ChipsetId_1, ledMatrixMax7219Control::Intensity_0);
   matrix.setBrightnessOfLEDMatrix(ledMatrixMax7219Control::ChipsetId_2, ledMatrixMax7219Control::Intensity_15);
   matrix.setBrightnessOfLEDMatrix(ledMatrixMax7219Control::ChipsetId_3, ledMatrixMax7219Control::Intensity_0);
@@ -51,41 +49,51 @@ void setup()
 
 }
 
-void moveBall(t_ball * ball)
+void moveBall()
 {
-  if ((ball->x += ball->xSpeed) < 0 || ball->x >= matrix.getScreenWidth())
+  // We add the horizontal speed to the ball. If the ball goes outside the screen,
+  // then we flip the speed and then add the speed to the position again
+  if ((ballX += ballXSpeed) < 0 || ballX >= matrix.getScreenWidth())
   {
-    ball->xSpeed *= -1;
-    ball->x += ball->xSpeed;
+    ballXSpeed *= -1;
+    ballX += ballXSpeed;
   }
-  if ((ball->y += ball->ySpeed) < 0 || ball->y >= matrix.getScreenHeight())
+  // We add the vertical speed to the ball. If the ball goes outside the screen,
+  // then we flip the speed and then add the speed to the position again
+  if ((ballY += ballYSpeed) < 0 || ballY >= matrix.getScreenHeight())
   {
-    ball->ySpeed *= -1;
-    ball->y += ball->ySpeed;
+    ballYSpeed *= -1;
+    ballY += ballYSpeed;
   }
 }
 
-void displayBall(t_ball * ball, uint8_t color)
+void displayBall(uint8_t color)
 {
-  matrix.setPixel(ball->x, ball->y, color);
+  // A ball is a single dot
+  matrix.setPixel(ballX, ballY, color);
 }
 
+// This function is called repeatedly
 void loop() 
 {
+  // We set the whole screen to ON every CountMax loops
   if (countLoop == CountMax)
   {
     matrix.clearScreen(1);
     countLoop = 0;
   }
-  
-  for (uint8_t i = 0; i < NbBalls; ++i)
-  {
-    displayBall(&balls[i], 0);
-    moveBall(&balls[i]);
-    displayBall(&balls[i], 1);
-  }
-  
+
+  // Remove the ball from screen (old position)
+  displayBall(0);
+  // Move the ball to its new position
+  moveBall();
+  // Set the ball on screen on its new position
+  displayBall(1);
+
+  // Refresh the screen
   matrix.displayScreen();
+  // Increment the loop counter, so it finaly reach CountMax
   ++countLoop;
+  // Sleep to temper the speed of the animation
   delay(10);
 }
